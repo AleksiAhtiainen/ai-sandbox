4d630479dd2a403500c2de36cbf615dbc1d14be2
Aleksi Ahtiainen <aleksi.ahtiainen@reaktor.com>
2026-04-27 14:19:15 +0300

    Lisää perustyökaluja ja copy-paste;
    Waylandin kanssa copy-paste ei toimi, koita disabloida.

--- configuration.nix
+++ configuration.nix
@@ -48,12 +48,16 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  
+  services.xserver.displayManager.gdm.wayland = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "fi";
     variant = "mac";
   };
+
+  services.spice-vdagentd.enable = true;
 
   # Configure console keymap
   console.keyMap = "fi";
@@ -99,6 +103,11 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    vim
+    curl
+    wget
+    git
+    spice-vdagent
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
