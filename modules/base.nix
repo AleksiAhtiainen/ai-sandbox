ec8069018ff2c276634f102193473c56c053793c
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2026-05-04 09:07:43 +0300

    Poista turhat printtaus ja äänet

--- modules/base.nix
+++ modules/base.nix
@@ -18,17 +18,6 @@
     LC_TIME = "fi_FI.UTF-8";
   };
 
-  services.printing.enable = true;
-
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
   programs.firefox.enable = true;
 
   nixpkgs.config.allowUnfree = true;
