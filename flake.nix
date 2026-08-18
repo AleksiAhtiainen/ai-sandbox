c6452ced557343629b7df69326a3797f8f0a0980
Aleksi Ahtiainen <Aleksi.Ahtiainen@reaktor.com>
2026-08-18 12:21:27 +0300

    Update to latest NixOS version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "AI Sandbox NixOS VM image";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nixos-generators = {
       url = "github:nix-community/nixos-generators";
